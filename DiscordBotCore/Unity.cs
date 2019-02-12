0d83d38de51217ae5fef668784c28f6de486628f
GreedyCoding <marco_hofer2012@outlook.com>
2019-02-12 23:05:47 +0100

    Implemented Unity for Unit Testing

---
+++ DiscordBotCore/Unity.cs
@@ -0,0 +1,34 @@
+﻿using DiscordBotCore.Storage;
+using DiscordBotCore.Storage.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace DiscordBotCore
+{
+    public static class Unity
+    {
+        private static UnityContainer _container;
+
+        public static UnityContainer Container
+        {
+            get
+            {
+                if(_container == null)
+                {
+                    RegisterTypes();
+                }
+                return _container;
+            }
+        }
+
+        public static void RegisterTypes()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<IDataStorage, InMemoryStorage>();
+        }
+    }
+}
