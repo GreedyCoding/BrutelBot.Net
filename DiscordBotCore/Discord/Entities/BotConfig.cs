b12c9e8fe6797b79289ec32af8396fa1d5c8cdf7
GreedyCoding <marco_hofer2012@outlook.com>
2019-03-30 23:29:09 +0100

    Added Discord configuration Classes

---
+++ DiscordBotCore/Discord/Entities/BotConfig.cs
@@ -0,0 +1,15 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotCore.Discord.Entities
+{
+    class BotConfig
+    {
+        public string Token { get; set; }
+        public DiscordSocketConfig SocketConfig { get; set; }
+    }
+}
