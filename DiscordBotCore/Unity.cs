6e2d46331cd1e216f9783fc00a24f7276caabfa8
GreedyCoding <marco_hofer2012@outlook.com>
2019-03-31 00:30:32 +0100

    Registered Logger type and addedd CCLM for Discord.Connection

--- DiscordBotCore/Unity.cs
+++ DiscordBotCore/Unity.cs
@@ -27,6 +27,8 @@ namespace DiscordBotCore
             _container = new UnityContainer();
             //ContainerControlledLifetimeManager makes sure only one InMemoryStorage will be used for all IDataStorages
             _container.RegisterType<IDataStorage, InMemoryStorage>(new ContainerControlledLifetimeManager());
+            _container.RegisterType<ILogger, Logger>(new ContainerControlledLifetimeManager());
+            _container.RegisterType<Discord.Connection>(new ContainerControlledLifetimeManager());
         }
 
         public static T Resolve<T>()
