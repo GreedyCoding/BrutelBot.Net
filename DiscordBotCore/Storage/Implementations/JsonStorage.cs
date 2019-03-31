ecdf933e2a28979835c06a6b4cc78cac89f3b229
GreedyCoding <marco_hofer2012@outlook.com>
2019-03-31 15:19:26 +0200

    Added JsonStorage Implementation

---
+++ DiscordBotCore/Storage/Implementations/JsonStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DiscordBotCore.Storage.Implementations
+{
+    public class JsonStorage : IDataStorage
+    {
+        public T RestoreObject<T>(string key)
+        {
+            var json = File.ReadAllText($"{key}.json");
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public void StoreObject(object obj, string key)
+        {
+            var file = $"{key}.json";
+            Directory.CreateDirectory(Path.GetDirectoryName(file));
+            var json = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(file, json);
+        }
+    }
+}
