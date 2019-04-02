92e9cfb2dee3fde16b013400a97f2a05e1ab2e96
GreedyCoding <marco_hofer2012@outlook.com>
2019-04-02 23:00:38 +0200

    Create Utilities.cs
    Added GetToken, SetToken, and ReturnInt for testing

---
+++ DiscordBotCore/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using DiscordBotCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotCore
+{
+    public class Utilities
+    {
+        static IDataStorage storage = Unity.Resolve<IDataStorage>();
+
+        public static string GetToken()
+        {
+            return storage.RestoreObject<string>("Config/BotToken");
+        }
+
+        public static void SetToken(string token)
+        {
+            storage.StoreObject(token, "Config/BotToken");
+        }
+
+        public static int ReturnInt(int i)
+        {
+            return i;
+        }
+    }
+}
