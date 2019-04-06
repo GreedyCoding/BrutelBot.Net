95d8365293b19651432cec4a9e72eb16ed0209d3
GreedyCoding <marco_hofer2012@outlook.com>
2019-04-06 22:22:34 +0200

    Implemented Basic Info and Say command module

---
+++ DiscordBotCore/Discord/Modules/SayModule.cs
@@ -0,0 +1,27 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotCore.Discord.Modules
+{
+    public class SayModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+	    [Summary("Echoes a message.")]
+	    public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+        {
+            if (echo.ToLower() == "gaymo" || echo.ToLower() == "gaym0")
+            {
+                ReplyAsync("Suck it!");
+            }
+            else
+            {
+                ReplyAsync(echo);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
