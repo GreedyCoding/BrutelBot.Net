2fb18dfa2988baef42101858e3b5f89f42b77e65
GreedyCoding <marco_hofer2012@outlook.com>
2019-04-07 14:40:26 +0200

    Created DeleteModule.cs

---
+++ DiscordBotCore/Discord/Modules/DeleteModule.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotCore.Discord.Modules
+{
+    public class DeleteModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("delete")]
+        [Summary("Deletes messages from a channel. Ammount can be specified as argument. Defaults to 10")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        public async Task DeleteMessageAsync(int amount = 10)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
