ddd115b55be30cd745074afb2c9a04f8b3d7776a
GreedyCoding <marco_hofer2012@outlook.com>
2019-04-07 14:41:00 +0200

    Changed InfoModule to HelpModule and extended it

---
+++ DiscordBotCore/Discord/Modules/HelpModule.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotCore.Discord.Modules
+{
+    public class HelpModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        [Summary("Displays information about the bot.")]
+        public async Task HelpAsync()
+        {
+            var embed = new EmbedBuilder
+            {
+                // Embed property can be set within object initializer
+                Title = "Help Window",
+                Description = "Displays all commands"
+            };
+            // Or with methods
+            embed.WithColor(Color.Blue)
+                 .WithTitle("Bot Help")
+                 .WithDescription("Below is a list with all currently available commands for BrutelBot")
+                 .AddField("Help", "!help - Shows bot information with all available commands")
+                 .AddField("Moderation", "!ban {user} {days}* {reason}* - Bans a User for specified amount of days\n" +
+                                         "!kick {user} {reason}* - Kicks a User\n" +
+                                         "!mute {user} {minutes}* {reason}* - Mutes a user for specified amount of minutes\n" +
+                                         "* Arguments are optional - days defaults to 1, minutes defaults to 15")
+                 .WithFooter(footer => footer.Text = "©BrutelBot")
+                 .WithCurrentTimestamp();
+
+            var info = embed.Build();
+
+            await ReplyAsync(embed: info);
+        }
+    }
+}
