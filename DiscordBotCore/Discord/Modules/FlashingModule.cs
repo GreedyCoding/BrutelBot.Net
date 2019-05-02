5cd4dde2a951480c8949f58de378d0cdf0316c4d
GreedyCoding <marco_hofer2012@outlook.com>
2019-05-02 22:53:52 +0200

    Implemented UserStatus Flashing via FlashingModule

---
+++ DiscordBotCore/Discord/Modules/FlashingModule.cs
@@ -0,0 +1,45 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotCore.Discord.Modules
+{
+    public class FlashingModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("flash")]
+        [Summary("Sets the bot userstatus to flashing between status")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        [RequireBotPermission(GuildPermission.Administrator)]
+        public async Task SetBlinking()
+        {
+            bool isBlinking = false;
+
+            if (!isBlinking)
+            {
+                isBlinking = true;
+            }
+            else
+            {
+                isBlinking = false;
+            }
+
+            await Context.Client.SetStatusAsync(UserStatus.Online);
+
+            while (isBlinking)
+            {
+                await Context.Client.SetStatusAsync(UserStatus.Online);
+                await Task.Delay(500);
+                await Context.Client.SetStatusAsync(UserStatus.Idle);
+                await Task.Delay(500);
+                await Context.Client.SetStatusAsync(UserStatus.DoNotDisturb);
+                await Task.Delay(500);
+            }
+
+        }
+    }
+}
