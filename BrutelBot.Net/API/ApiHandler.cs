21d7c45593f4beb8a72ec803251b20e9632d6ca8
GreedyCoding <marco_hofer2012@outlook.com>
2019-05-19 21:28:35 +0200

    Added ApiHandler and implemented an XkcdModule which posts a comic

---
+++ BrutelBot.Net/API/ApiHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrutelBot.API
+{
+    public static class ApiHandler
+    {
+        public static HttpClient ApiClient { get; set; }
+         
+        static ApiHandler()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ BrutelBot.Net/Discord/Modules/XkcdModule.cs
@@ -0,0 +1,23 @@
+﻿using BrutelBot.API;
+using BrutelBot.API.Xkcd;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrutelBot.Discord.Modules
+{
+    public class XkcdModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("xkcd")]
+        public async Task GetComic(int comicNumber = 0)
+        {
+            ComicModel comic = await ComicHandler.LoadComic(comicNumber);
+            await ReplyAsync(comic.Img);
+        }
+    }
+}
